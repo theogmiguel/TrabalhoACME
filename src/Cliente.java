b6f5d07e0eb350d88b1f616abb57fd491f778406
Theo Miguel <theogmiguel@gmail.com>
2023-08-29 00:36:57 -0300

    Primeiro commit, T1 de POO

---
+++ src/Cliente.java
@@ -0,0 +1,11 @@
+public class Cliente {
+    private String email;
+    private String nome;
+    private String endereco;
+
+    public boolean adicionarEntrega (Entrega entrega){
+        return false;
+    }
+
+
+}
---
+++ src/Entrega.java
@@ -0,0 +1,11 @@
+public class Entrega {
+    private int codigo;
+    private double valor;
+    private String descricao;
+
+    public Entrega(int codigo, double valor, String descricao) {
+        this.codigo = codigo;
+        this.valor = valor;
+        this.descricao = descricao;
+    }
+}
