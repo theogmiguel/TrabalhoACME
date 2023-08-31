38c239cd22f14d5f5afa5c0ce58edcb0a7b3c805
Theo Miguel <theogmiguel@gmail.com>
2023-08-30 22:07:09 -0300

    Terceiro commit
    Scanner e leitor de arquivos criados e algumas das classes feitas.

--- src/ACMEDelivery.java
+++ src/ACMEDelivery.java
@@ -10,9 +10,11 @@ public class ACMEDelivery {
 
 	private CadastroEntregas cadastroEntregas;
 	private Clientela clientela;
-	private Scanner entrada;
+	private Scanner entrada = null;                  // Atributo para entrada de dados
+	private PrintStream saidaPadrao = System.out;	 // 	Guarda a saida padrão - tela (console)
+	private Clientela novaClientela;
 
-	public void executa() {
+	public ACMEDelivery() {
 		try {
 			BufferedReader streamEntrada = new BufferedReader(new FileReader("arquivoentrada.txt"));
 			entrada = new Scanner(streamEntrada);   // Usa como entrada um arquivo
@@ -24,8 +26,31 @@ public class ACMEDelivery {
 		Locale.setDefault(Locale.ENGLISH);   // Ajusta para ponto decimal
 		entrada.useLocale(Locale.ENGLISH);
 
-
+		novaClientela = new Clientela();
 	}
 
+	public void executa() {
+		adicionarClientes();
+	}
 
+	private void adicionarClientes() {
+		String email = "";
+		String nome = "";
+		String endereco = "";
+
+		email = entrada.nextLine();
+		do {
+			nome = entrada.nextLine();
+			endereco = entrada.nextLine();
+			entrada.nextLine();
+
+			Cliente novoCliente = new Cliente(email, nome, endereco);
+			novaClientela.cadastraCliente(novoCliente);
+
+			System.out.println(email);
+			System.out.println(nome);
+			System.out.println(endereco);
+
+		} while (!email.equals("-1"));
+	}
 }
